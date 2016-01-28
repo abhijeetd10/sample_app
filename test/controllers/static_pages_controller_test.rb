4867e2811ad81e27a94a801b73af29578a4ff346
Abhijeet <abhijeet.kumar@nestaway.com>
2016-01-28 16:15:18 +0530

    Lesson 5 Completed

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,12 +10,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Sample_APP"
+    assert_select "title", "Help | Sample_APP"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "Sample_APP"
+    assert_select "title", "About | Sample_APP"
+  end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Sample_APP"
   end
 end
