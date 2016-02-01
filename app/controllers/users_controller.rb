4a6ce6a786744a665974e04955aee6603605bf9b
Abhijeet <abhijeet.kumar@nestaway.com>
2016-02-01 17:23:35 +0530

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 	def create
 	@user = User.new(user_params)
 	if @user.save
+	log_in @user
 	flash[:success] = "Welcome to the Sample App!"	
 	redirect_to @user
 	else
@@ -16,7 +17,6 @@ class UsersController < ApplicationController
 	end
 	private
 	def user_params
-	params.require(:user).permit(:name, :email, :password,
-	:password_confirmation)
+	params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
 end
